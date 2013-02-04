8927478ff36a8068ca39a637335ea47ef4f2bd26
Cam Moore <cammymoore07@gmail.com>
2013-02-04 11:12:54 -1000

    have row and column info for the dropped actions.

--- makahiki/apps/widgets/sgg_design/views.py
+++ makahiki/apps/widgets/sgg_design/views.py
@@ -1,5 +1,12 @@
 """Provides the view of the widget."""
 from apps.widgets.smartgrid.models import Level, Category, Activity, Event, Commitment
+from django.views.decorators.cache import never_cache
+from django.contrib.auth.decorators import login_required
+from apps.widgets.smartgrid import smartgrid
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 
 def supply(request, page_name):
@@ -19,3 +26,20 @@ def supply(request, page_name):
         'events': events,
         'commitments': commitments,
             }
+
+
+@never_cache
+@login_required
+def view_action(request, action_type, slug):
+    """individual action page"""
+    action = smartgrid.get_action(slug=slug)
+    user = request.user
+    view_objects = {}
+
+    action = smartgrid.annotate_action_details(user, action)
+
+    return render_to_response("action.html", {
+        "action": action,
+        "display_form": True if "display_form" in request.GET else False,
+        "view_objects": view_objects,
+        }, context_instance=RequestContext(request))
