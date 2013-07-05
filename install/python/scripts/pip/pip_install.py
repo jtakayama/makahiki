1d84be0aa6684e75dda25ec339e3def4db941525
jtakayama <j.k.takayama@gmail.com>
2013-07-05 13:00:53 -1000

    Pushing working version of pip/pip_install.py

--- install/python/scripts/pip/pip_install.py
+++ install/python/scripts/pip/pip_install.py
@@ -35,18 +35,22 @@ def requirements_check(logfile):
     installed_list = installed.split("\n")
     # Search in list of installed requirements for matches to entries in requirements.txt
     for element in requirements_list:
-        element_regex = re.compile("(%s)(.)*" % element)
+        element_regex = re.compile("(.*)(%s)(.*)" % element)
         element_installed = False
         for element2 in installed_list:
             if element_regex.match(element2):
                 element_installed = True
                 break
-        if element_installed:
-            logfile.write("%s installed successfully." % element)
-            print ("%s installed successfully." % element)
-        else:
-            logfile.write("Warning: %s not installed." % element)
-            print ("Warning: %s not installed." % element)
+	    else:
+                continue
+        # Avoid printing empty list elements
+        if element != "":
+            if element_installed:
+                logfile.write("%s installed successfully." % element)
+                print ("%s installed successfully." % element)
+            else:
+                logfile.write("Warning: %s not installed." % element)
+                print ("Warning: %s not installed." % element)
     return logfile
     
 def run(logfile):
@@ -60,16 +64,25 @@ def run(logfile):
         USER_PROJECT_HOME = USER_HOME + "/makahiki"
         # cd to makahiki directory so pip can find the requirements.txt file
         os.chdir(USER_PROJECT_HOME)
-        pip_output = subprocess.check_output(["pip install", "-r", "requirements.txt"], stderr=subprocess.STDOUT, shell=True)
+        pip_output = subprocess.check_output(["pip install -r requirements.txt"], stderr=subprocess.STDOUT, shell=True)
         logfile.write(pip_output)
         print(pip_output)
         # pip produces a lot of output. Clear the buffer before reading in anything else.
         logfile.flush()
         os.fsync(logfile)
         # Check that requirements were installed
+        output1 = "\nChecking if dependencies in requirements.txt were installed:\n"
+        logfile.write(output1)
+        print output1
         logfile = requirements_check(logfile)
+        # Print a closing message
+        output2 = "\npip install script has completed.\n"
+        logfile.write(output2)
+        print output2
+        end_time = termination_string()
+        logfile.write(end_time)
+        print end_time
         return logfile
-        
     except subprocess.CalledProcessError as cpe:
         logfile.write("CalledProcessError: ")
         print "CalledProcessError: "
