44ed2790313e4395209fdab143861c0cedec7159
jtakayama <j.k.takayama@gmail.com>
2013-07-06 21:00:17 -1000

    Committing first working version of install/run_update_instance.py file.

--- install/run_update_instance.py
+++ install/run_update_instance.py
@@ -20,20 +20,39 @@ def run(logfile):
         USER_PROJECT_HOME = USER_HOME + "/makahiki"
         # cd to makahiki directory so pip can find the requirements.txt file
         os.chdir(USER_PROJECT_HOME)
-        update_output = subprocess.check_output(["python", "./makahiki/scripts/update_instance.py"], stderr=subprocess.STDOUT, shell=True)
+        update_output = subprocess.check_output(["python ./makahiki/scripts/update_instance.py"], stderr=subprocess.STDOUT, shell=True)
         logfile.write(update_output)
         print(update_output)
         # Clear the buffer.
         logfile.flush()
         os.fsync(logfile)
+        # Print a closing message
+        closing = "\nMakahiki instance update script has completed.\n"
+        logfile.write(closing)
+        print closing
+        end_time = termination_string()
+        logfile.write(end_time)
+        print end_time
+        return logfile
     except subprocess.CalledProcessError as cpe:
         logfile.write("CalledProcessError: ")
         print "CalledProcessError: "
         logfile.write(cpe.output)
         print cpe.output
-        logfile.write("Warning: update_instance did not complete successfully.")
-        print "Warning: update_instance did not complete successfully."
+        logfile.write("Warning: Makahiki instance update did not complete successfully.")
+        print "Warning: Makahiki instance update did not complete successfully."
         end_time = termination_string()
         logfile.write(end_time)
         print end_time
         return logfile 
+    except OSError as ose:
+        logfile.write("OSError: ")
+        print "OSError: "
+        logfile.write(ose.output)
+        print ose.output
+        logfile.write("Warning: Makahiki instance update did not complete successfully.")
+        print "Warning: Makahiki instance update did not complete successfully."
+        end_time = termination_string()
+        logfile.write(end_time)
+        print end_time
+        return logfile
