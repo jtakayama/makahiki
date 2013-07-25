f11ee6476a849d31374382cf226976fe059c61da
jtakayama <j.k.takayama@gmail.com>
2013-07-25 03:13:27 -1000

    run_bootstrap.sh files for Ubuntu x86 and x64: Changed file paths to absolute paths.

--- install/vagrant_scripts/ubuntu_x64/run_bootstrap.sh
+++ install/vagrant_scripts/ubuntu_x64/run_bootstrap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+# Sync virtual machine time with real time before creating file timestamp
+sudo ntpdate pool.ntp.org
 FNAME="ubuntu_x64_"
 TIMESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
 FNAME2="$FNAME$TIMESTAMP.log"
-sudo sh bootstrap.sh | tee "logs/$FNAME2"
+# Run installation script
+sudo sh /vagrant/bootstrap.sh | tee "/vagrant/logs/$FNAME2"
 
