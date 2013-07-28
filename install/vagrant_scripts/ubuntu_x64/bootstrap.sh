cb3c9dc80b9678351d216b5668fe77b427adca7d
jtakayama <j.k.takayama@gmail.com>
2013-07-28 08:59:15 -1000

    install/vagrant_scripts/ubuntu_x86/bootstrap.sh, install/vagrant_scripts/ubuntu_x64/bootstrap.sh: Fixed mixed tabs-and-spaces in if statements --> four spaces per tab.

--- install/vagrant_scripts/ubuntu_x64/bootstrap.sh
+++ install/vagrant_scripts/ubuntu_x64/bootstrap.sh
@@ -43,39 +43,39 @@ apt-get install -y libjpeg-dev
 # Python Imaging Library shared library symlink setup
 echo "Checking locations of Python Imaging Library shared libraries."
 if [ ! -f /usr/lib/libjpeg.so ]
-	then
-	    if [ -f /usr/lib/x86_64-linux-gnu/libjpeg.so ]
-			then
-				echo "Found: /usr/lib/x86_64-linux-gnu/libjpeg.so"
-				echo "Creating symlink: /usr/lib/libjpeg.so --> /usr/lib/x86_64-linux-gnu/libjpeg.so"
-				echo "sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib/libjpeg.so"
-				sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib/
-			else
-				echo "Could not find libjpeg.so in /usr/lib or /usr/lib/x86_64-linux-gnu/." 
-				echo "Python Imaging Library packages may not have installed properly."
-				echo "Script exiting at $(date)."
-				exit 1
-		fi
-	else
-		echo "Found: /usr/lib/libjpeg.so"
+    then
+        if [ -f /usr/lib/x86_64-linux-gnu/libjpeg.so ]
+            then
+                echo "Found: /usr/lib/x86_64-linux-gnu/libjpeg.so"
+                echo "Creating symlink: /usr/lib/libjpeg.so --> /usr/lib/x86_64-linux-gnu/libjpeg.so"
+                echo "sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib/libjpeg.so"
+                sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib/
+            else
+                echo "Could not find libjpeg.so in /usr/lib or /usr/lib/x86_64-linux-gnu/." 
+                echo "Python Imaging Library packages may not have installed properly."
+                echo "Script exiting at $(date)."
+                exit 1
+        fi
+    else
+        echo "Found: /usr/lib/libjpeg.so"
 fi
 
 if [ ! -f /usr/lib/libz.so ]
-	then
-		if [ -f /usr/lib/x86_64-linux-gnu/libz.so ]
-			then
-				echo "Found: /usr/lib/x86_64-linux-gnu/libz.so"
-				echo "Creating symlink: /usr/lib/libz.so --> /usr/lib/x86_64-linux-gnu/libz.so"
-				echo "sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib/"
-				sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib/
-			else
-				echo "Could not find libz.so in /usr/lib or /usr/lib/x86_64-linux-gnu/."
-				echo "Python Imaging Library packages may not have installed properly."
-				echo "Script exiting at $(date)"
-				exit 1
-		fi
-	else
-		echo "Found: /usr/lib/libz.so"
+    then
+        if [ -f /usr/lib/x86_64-linux-gnu/libz.so ]
+            then
+                echo "Found: /usr/lib/x86_64-linux-gnu/libz.so"
+                echo "Creating symlink: /usr/lib/libz.so --> /usr/lib/x86_64-linux-gnu/libz.so"
+                echo "sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib/"
+                sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib/
+            else
+                echo "Could not find libz.so in /usr/lib or /usr/lib/x86_64-linux-gnu/."
+                echo "Python Imaging Library packages may not have installed properly."
+                echo "Script exiting at $(date)"
+                exit 1
+        fi
+    else
+        echo "Found: /usr/lib/libz.so"
 fi
 echo "Python Imaging Library shared libraries check is complete."
 echo "Installing postgresql-9.1."
