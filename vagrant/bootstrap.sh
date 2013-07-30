e4087cdd26256158b4db036ec0f261e4ab1a22b9
jtakayama <j.k.takayama@gmail.com>
2013-07-30 12:41:55 -1000

    vagrant/: Removed x64 virtual machine instructions, and moved ubuntu_x86/ contents to vagrant/.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "Makahiki Environment Setup Script for Ubuntu (x64)"
+echo "Makahiki Environment Setup Script for Ubuntu (x86)"
 echo "--------------------------------------------------"
 echo "Script started at $(date)"
 echo "Appending locale settings (UTF-8) to /etc/bash.bashrc: started at $(date)"
@@ -7,11 +7,11 @@ echo "# UTF-8 locale settings for Makahiki" >> /etc/bash.bashrc
 echo "export LANGUAGE=en_US.UTF-8" >> /etc/bash.bashrc
 echo "export LANG=en_US.UTF-8" >> /etc/bash.bashrc
 echo "export LC_ALL=en_US.UTF-8" >> /etc/bash.bashrc
-echo "Appending locale settings (UTF-8) to /etc/bash.bashrc: finished at $(date)"
-echo "Configuring locale settings: started at $(date)"
+echo "Appending locale settings (UTF-8): finished at $(date)"
+echo "Configuring UTF-8 locale settings: started at $(date)"
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
-echo "Configuring locale settings: finished at $(date)"
+echo "Configuring UTF-8 locale settings: finished at $(date)"
 echo "Updating package list: started at $(date)"
 echo "apt-get update"
 apt-get update
@@ -25,7 +25,7 @@ echo "apt-get install -y gcc"
 apt-get install -y gcc
 echo "Installing gcc: finished at $(date)"
 echo "Installing python-setuptools: started at $(date)"
-echo "apt-get install -y python-setuptools" 
+echo "apt-get install -y python-setuptools"
 apt-get install -y python-setuptools
 echo "Installing python-setuptools: finished at $(date)"
 echo "Installing pip: started at $(date)"
@@ -48,32 +48,32 @@ echo "Installing libjpeg-dev: finished at $(date)"
 echo "Configuring Python Imaging Library shared libraries: started at $(date)"
 if [ ! -f /usr/lib/libjpeg.so ]
     then
-        if [ -f /usr/lib/x86_64-linux-gnu/libjpeg.so ]
+        if [ -f /usr/lib/i386-linux-gnu/libjpeg.so ]
             then
-                echo "Found: /usr/lib/x86_64-linux-gnu/libjpeg.so"
-                echo "Creating symlink: /usr/lib/libjpeg.so --> /usr/lib/x86_64-linux-gnu/libjpeg.so"
-                echo "sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib/libjpeg.so"
-                sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib/
+                echo "Found: /usr/lib/i386-linux-gnu/libjpeg.so"
+                echo "Creating symlink: /usr/lib/libjpeg.so --> /usr/lib/i386-linux-gnu/libjpeg.so"
+                echo "sudo ln -s /usr/lib/i386-linux-gnu/libjpeg.so /usr/lib/libjpeg.so"
+                sudo ln -s /usr/lib/i386-linux-gnu/libjpeg.so /usr/lib/
             else
-                echo "Could not find libjpeg.so in /usr/lib or /usr/lib/x86_64-linux-gnu/." 
+                echo "Could not find libjpeg.so in /usr/lib or /usr/lib/i386-linux-gnu/." 
                 echo "Python Imaging Library packages may not have installed properly."
                 echo "Script exiting at $(date)."
                 exit 1
-        fi
+                fi
     else
         echo "Found: /usr/lib/libjpeg.so"
 fi
 
 if [ ! -f /usr/lib/libz.so ]
     then
-        if [ -f /usr/lib/x86_64-linux-gnu/libz.so ]
+        if [ -f /usr/lib/i386-linux-gnu/libz.so ]
             then
-                echo "Found: /usr/lib/x86_64-linux-gnu/libz.so"
-                echo "Creating symlink: /usr/lib/libz.so --> /usr/lib/x86_64-linux-gnu/libz.so"
-                echo "sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib/"
-                sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib/
+                echo "Found: /usr/lib/i386-linux-gnu/libz.so"
+                echo "Creating symlink: /usr/lib/libz.so --> /usr/lib/i386-linux-gnu/libz.so"
+                echo "sudo ln -s /usr/lib/i386-linux-gnu/libz.so /usr/lib/"
+                sudo ln -s /usr/lib/i386-linux-gnu/libz.so /usr/lib/
             else
-                echo "Could not find libz.so in /usr/lib or /usr/lib/x86_64-linux-gnu/."
+                echo "Could not find libz.so in /usr/lib or /usr/lib/i386-linux-gnu/."
                 echo "Python Imaging Library packages may not have installed properly."
                 echo "Script exiting at $(date)"
                 exit 1
@@ -96,6 +96,12 @@ pg_dropcluster 9.1 main --stop
 echo "pg_createcluster --locale en_US.UTF8 9.1 main"
 pg_createcluster --locale en_US.UTF8 9.1 main
 echo "Configuring PostgreSQL cluster \"main\" with locale en_US.UTF8: finished at $(date)"
+echo "Copying PostgreSQL 9.1 config: started at $(date)"
+echo "sh /home/vagrant/copy_config/md5test_ubuntu_pg_hba_conf.sh"
+sh /home/vagrant/copy_config/md5test_ubuntu_pg_hba_conf.sh"
+echo "/etc/init.d/postgresql-9.1 restart"
+/etc/init.d/postgresql-9.1 restart
+echo "Copying PostgreSQL 9.1 config: finished at $(date)"
 echo "Installing memcached: started at $(date)"
 echo "apt-get install -y memcached"
 apt-get install -y memcached
@@ -123,5 +129,21 @@ git clone http://github.com/jtakayama/makahiki.git
 echo "chown -R vagrant:vagrant /home/vagrant/makahiki"
 chown -R vagrant:vagrant /home/vagrant/makahiki
 echo "Downloading source code from Github: finished at $(date)"
+echo "Creating virtualenv: started at $(date)"
+echo "cd /home/vagrant/makahiki"
+cd /home/vagrant/makahiki
+echo "mkvirtualenv makahiki"
+mkvirtualenv makahiki
+echo "Creating virtualenv: finished at $(date)"
+echo "pip install: started at $(date)"
+echo "pip install -r requirements.txt"
+pip install -r requirements.txt
+echo "pip install: finished at $(date)"
+echo "Setting Makahiki environment variables: started at $(date)"
+echo "sh /home/vagrant/copy_config/md5test_ubuntu_postactivate.sh"
+sh /home/vagrant/copy_config/md5test_ubuntu_postactivate.sh
+echo "workon makahiki"
+workon makahiki
+echo "Setting Makahiki environment variables: finished at $(date)"
 echo "Script completed at $(date)"
 exit 0
