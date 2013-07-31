a7f16b21ba4ac43e09d6239abd0a8cde567f6342
jtakayama <j.k.takayama@gmail.com>
2013-07-31 01:05:15 -1000

    Changed configuration process to automate more steps, mostly in editing of bashrc files.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,15 +1,50 @@
 #!/usr/bin/env bash
+BASH_BASHRC_COPY_RESULT = "Unknown"
+PG_HBA_CONF_COPY_RESULT = "Unknown"
+MAKAHIKI_ENV_SETUP_RESULT = "Unknown"
+
 echo "Makahiki Environment Setup Script for Ubuntu (x86)"
 echo "--------------------------------------------------"
 echo "Script started at $(date)"
-echo "Appending locale settings (UTF-8) to /etc/bash.bashrc: started at $(date)"
-echo "# UTF-8 locale settings for Makahiki" >> /etc/bash.bashrc
-echo "export LANGUAGE=en_US.UTF-8" >> /etc/bash.bashrc
-echo "export LANG=en_US.UTF-8" >> /etc/bash.bashrc
-echo "export LC_ALL=en_US.UTF-8" >> /etc/bash.bashrc
-echo "Appending locale settings (UTF-8): finished at $(date)"
+echo "Copying bash.bashrc with locale settings (UTF-8) to /etc/bash.bashrc: started at $(date)"
+# Begin bash.bashrc copying code:
+MD5SUM_BASH_EXPECTED=$(md5sum "/vagrant/vagrant/config_examples/bash.bashrc.ubuntu.default")
+MD5SUM_BASH_ACTUAL=$(md5sum "/etc/bash.bashrc.ubuntu.default")
+MD5SUM_BASH_MAKAHIKI=$(md5sum "/vagrant/vagrant/config_examples/bash.bashrc.ubuntu.makahiki")
+
+# Split string <checksum><2 spaces><filename> on spaces (awk default)
+MD5_BASH_EXPECTED=$(echo "$MD5SUM_BASH_EXPECTED" | awk '{ print $1 }')
+MD5_BASH_ACTUAL=$(echo "$MD5SUM_BASH_ACTUAL" | awk '{ print $1 }')
+MD5_BASH_MAKAHIKI=$(echo "$MD5SUM_BASH_MAKAHIKI" | awk '{ print $1 }')
+
+echo "Expected md5 checksum of default PostgreSQL 9.1 pg_hba.conf: $MD5_BASH_EXPECTED"
+echo "Actual md5 checksum of default PostgreSQL 9.1 pg_hba.conf: $MD5_BASH_ACTUAL"
+
+if [ $MD5_BASH_EXPECTED = $MD5_BASH_ACTUAL ]
+    then
+        echo "Checksums match. bash.bashrc will be overwritten with Makahiki settings."
+        echo "sudo cp /vagrant/vagrant/config_examples/bash.bashrc.ubuntu.makahiki /etc/bash.bashrc"
+        sudo cp /vagrant/vagrant/config_examples/bash.bashrc.ubuntu.makahiki /etc/bash.bashrc
+        echo "bash.bashrc copy succeeded. [ OK ]"
+        export BASH_BASHRC_COPY_RESULT="Succeeded"
+    else
+        if [ $MD5_BASH_MAKAHIKI = $MD5_BASH_ACTUAL ]
+            then
+                echo "bash.bashrc file already overwritten with makahiki settings. [ OK ]"
+                export BASH_BASHRC_COPY_RESULT="Already completed" 
+            else
+                echo "WARNING! bash.bashrc default file is different from expected file."
+                echo "File could not be safely overwritten with Makahiki defaults."
+                echo "You will need to edit it manually."
+                export BASH_BASHRC_COPY_RESULT="Failed"
+        fi
+fi
+# End bash.bashrc copying code.
+echo "Copying bash.bashrc with locale settings (UTF-8) to /etc/bash.bashrc: finished at $(date)"
 echo "Configuring UTF-8 locale settings: started at $(date)"
+echo "locale-gen en_US.UTF-8"
 locale-gen en_US.UTF-8
+echo "dpkg-reconfigure locales"
 dpkg-reconfigure locales
 echo "Configuring UTF-8 locale settings: finished at $(date)"
 echo "Updating package list: started at $(date)"
@@ -96,8 +131,8 @@ pg_dropcluster 9.1 main --stop
 echo "pg_createcluster --locale en_US.UTF8 9.1 main"
 pg_createcluster --locale en_US.UTF8 9.1 main
 echo "Configuring PostgreSQL cluster \"main\" with locale en_US.UTF8: finished at $(date)"
-echo "Copying PostgreSQL 9.1 pg_hba.conf: started at $(date)"
 # Begin pg_hba.conf copying code:
+echo "Copying PostgreSQL 9.1 pg_hba.conf: started at $(date)"
 MD5SUM_PGHBA_EXPECTED=$(md5sum "/vagrant/vagrant/config_examples/pg_hba.conf.ubuntu.default")
 MD5SUM_PGHBA_ACTUAL=$(md5sum "/etc/postgresql/9.1/main/pg_hba.conf")
 MD5SUM_PGHBA_MAKAHIKI=$(md5sum "/vagrant/vagrant/config_examples/pg_hba.conf.ubuntu.makahiki")
@@ -116,20 +151,23 @@ if [ $MD5_PGHBA_EXPECTED = $MD5_PGHBA_ACTUAL ]
         echo "sudo cp /vagrant/vagrant/config_examples/pg_hba.conf.ubuntu.makahiki /etc/postgresql/9.1/main/pg_hba.conf"
         sudo cp /vagrant/vagrant/config_examples/pg_hba.conf.ubuntu.makahiki /etc/postgresql/9.1/main/pg_hba.conf
         echo "pg_hba.conf copy succeeded. [ OK ]"
+        export PG_HBA_CONF_COPY_RESULT="Succeeded"
     else
         if [ $MD5_PGHBA_MAKAHIKI = $MD5_PGHBA_ACTUAL ]
             then
                 echo "pg_hba.conf file already overwritten with makahiki settings. [ OK ]"
+                export PG_HBA_CONF_COPY_RESULT="Already completed"
             else
                 echo "WARNING! pg_hba.conf default file is different from expected file."
                 echo "File could not be safely overwritten with Makahiki defaults."
                 echo "You will need to edit it manually."
+                export PG_HBA_CONF_COPY_RESULT="Failed"
         fi
 fi
-# End pg_hba.conf copying code.
 echo "/etc/init.d/postgresql-9.1 restart"
 sudo /etc/init.d/postgresql-9.1 restart
 echo "Copying PostgreSQL 9.1 pg_hba.conf: finished at $(date)"
+# End pg_hba.conf copying code.
 echo "Installing memcached: started at $(date)"
 echo "apt-get install -y memcached"
 apt-get install -y memcached
@@ -150,21 +188,64 @@ if [ ! -f /home/vagrant/makahiki_env.sh ]
         echo "chown vagrant:vagrant /home/vagrant/makahiki_env.sh"
         chown vagrant:vagrant /home/vagrant/makahiki_env.sh
         echo "Appending Makahiki environment variables..."
+        echo "# Makahiki environment variables" >> /home/vagrant/makahiki_env.sh
         echo "# Syntax: postgres://<db_user>:<db_password>@<db_host>:<db_port>/<db_name>" >> /home/vagrant/makahiki_env.sh
         echo "export MAKAHIKI_DATABASE_URL=postgres://makahiki:makahiki@localhost:5432/makahiki" >> /home/vagrant/makahiki_env.sh
         echo "# Syntax: <admin_name>:<admin_password>" >> /home/vagrant/makahiki_env.sh
         echo "export MAKAHIKI_ADMIN_INFO=admin:admin" >> /home/vagrant/makahiki_env.sh
         echo "Creating /home/vagrant/makahiki_env.sh: finished at $(date)"
-        echo "Appending Makahiki settings to /home/vagrant/.bashrc: started at $(date)"
-        echo "source /home/vagrant/makahiki_env.sh" >> /home/vagrant/.bashrc
-        echo "Appending Makahiki settings to /home/vagrant/.bashrc: finished at $(date)"   
+        MAKAHIKI_ENV_SETUP_RESULT="Succeeded."
 else
-    echo "/home/vagrant/makahiki_env.sh already exists. No changes will be made to .bashrc."
+    echo "/home/vagrant/makahiki_environment.sh already exists. [ OK ]"
+    MAKAHIKI_ENV_SETUP_RESULT="Already completed."
+# Begin .bashrc appending code:
+echo "Appending Makahiki settings to /home/vagrant/.bashrc: started at $(date)"
+MD5SUM_HOME_BASHRC_EXPECTED=$(md5sum "/vagrant/vagrant/config_examples/pg_hba.conf.ubuntu.default")
+MD5SUM_HOME_BASHRC_ACTUAL=$(md5sum "/etc/postgresql/9.1/main/pg_hba.conf")
+MD5SUM_HOME_BASHRC_MAKAHIKI=$(md5sum "/vagrant/vagrant/config_examples/pg_hba.conf.ubuntu.makahiki")
+
+# Split string <checksum><2 spaces><filename> on spaces (awk default)
+MD5_HOME_BASHRC_EXPECTED=$(echo "$MD5SUM_HOME_BASHRC_EXPECTED" | awk '{ print $1 }')
+MD5_HOME_BASHRC_ACTUAL=$(echo "$MD5SUM_HOME_BASHRC_ACTUAL" | awk '{ print $1 }')
+MD5_HOME_BASHRC_MAKAHIKI=$(echo "$MD5SUM_HOME_BASHRC_MAKAHIKI" | awk '{ print $1 }')
+
+echo "Expected md5 checksum of default PostgreSQL 9.1 pg_hba.conf: $MD5_HOME_BASHRC_EXPECTED"
+echo "Actual md5 checksum of default PostgreSQL 9.1 pg_hba.conf: $MD5_HOME_BASHRC_ACTUAL"
+
+if [ $MD5_HOME_BASHRC_EXPECTED = $MD5_HOME_BASHRC_ACTUAL ]
+    then
+        echo "Checksums match. /home/vagrant/.bashrc will have Makahiki settings appended."
+        echo "# Makahiki environment settings" >> /home/vagrant/.bashrc
+        echo "source /home/vagrant/makahiki_env.sh" >> /home/vagrant/.bashrc
+        echo "/home/vagrant/.bashrc append succeeded. [ OK ]"
+        export HOME_BASHRC_RESULT="Succeeded"
+    else
+        if [ $MD5_HOME_BASHRC_MAKAHIKI = $MD5_HOME_BASHRC_ACTUAL ]
+            then
+                echo "/home/vagrant/.bashrc file already overwritten with makahiki settings. [ OK ]"
+                export HOME_BASHRC_RESULT="Already completed"
+            else
+                echo "WARNING! /home/vagrant/.bashrc default file is different from expected file."
+                echo "File could not be safely overwritten with Makahiki defaults."
+                echo "You will need to edit it manually."
+                export HOME_BASHRC_RESULT="Failed"
+        fi
+fi
+echo "Appending Makahiki settings to /home/vagrant/.bashrc: finished at $(date)"
+# End .bashrc appending code.
 echo "pip install: started at $(date)"
 echo "cd /home/vagrant/makahiki"
 cd /home/vagrant/makahiki
 echo "pip install -r requirements.txt"
 pip install -r requirements.txt
 echo "pip install: finished at $(date)"
+echo "-------------------------------------------------------------------------------"
+echo "Configuration setup results:"
+echo "-------------------------------------------------------------------------------"
+echo "1. Copying locale settings to /etc/bash.bashrc: [$BASH_BASHRC_COPY_RESULT]"
+echo "2. Copying settings to pg_hba.conf: [$PG_HBA_CONF_COPY_RESULT]"
+echo "3. Creating /home/vagrant/makahiki_env.sh: [$MAKAHIKI_ENV_SETUP_RESULT]"
+echo "4. Appending to /home/vagrant/.bashrc: [$HOME_BASHRC_RESULT]"
+echo "-------------------------------------------------------------------------------"
 echo "Script completed at $(date)"
 exit 0
