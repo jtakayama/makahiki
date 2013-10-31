4308a2a4eef3d49a1c5c4ee401b7f78d96973bcc
jtakayama <j.k.takayama@gmail.com>
2013-10-31 11:29:47 -1000

    makahiki/apps/widgets/enable_games: Committing first working version of enable_games widget.

--- makahiki/apps/widgets/enable_games/views.py
+++ makahiki/apps/widgets/enable_games/views.py
@@ -1,13 +1,5 @@
 """Provides the view of the widget."""
 
-from django.shortcuts import render_to_response
-from django.shortcuts import render
-#from django.http import HttpResponseRedirect
-
-#games_enabled = None
-#games_disabled = None
-
-
 def supply(request, page_name):
     """ supply view_objects for widget rendering."""
     _ = request
@@ -24,15 +16,14 @@ def supply(request, page_name):
         games_to_enable = request.POST.getlist("disabled_game[]")
         if len(games_to_disable) > 0:
             for game in games_to_disable:
-                # This will cause an error if more than one object has the same name
+                # Assume games have unique names.
                 current_game = GameInfo.objects.get(name=game)
                 current_game.enabled = False
                 current_game.save()
         # Enable disabled apps if checked
-        if len(games_to_disable) > 0:
-            print "Execution check, remove when done."
+        if len(games_to_enable) > 0:
             for game in games_to_enable:
-                # This will cause an error if more than one object has the same name
+                # Assume that games have unique names.
                 current_game = GameInfo.objects.get(name=game)
                 current_game.enabled = True
                 current_game.save()
@@ -48,71 +39,4 @@ def supply(request, page_name):
         "games_enabled": games_enabled,
         "games_disabled": games_disabled
     }
-
-#def supply(request, page_name):
-#    """ supply view_objects for widget rendering."""
-#    _ = request
-#    _ = page_name
-#    
-#    from apps.managers.challenge_mgr.models import GameInfo
-#    from django.forms.models import modelformset_factory
-#    GameInfoFormSet = modelformset_factory(GameInfo, fields=('name','enabled'))
-#    
-#    if request.method == 'POST':
-#        formset = GameInfoFormSet(request.POST, request.FILES,
-#                                  queryset=GameInfo.objects.filter(enabled=True))
-#        if formset.is_valid():
-#            formset.save()
-#    
-#    else:
-#        formset = GameInfoFormSet()
-
-#    return render_to_response('quick_start.html', {"formset": formset})
-    #return {"formset": formset,}
-        #"games_enabled": games_enabled,
-        #"games_disabled": games_disabled
-    #}
-
-#from django.forms import ModelForm
-#from apps.managers.challenge_mgr.models import GameInfo
-#from django.forms import CheckboxInput
-
-#class SetGameInfoEnableForm(ModelForm):
-#    is_enabled = list(challenge_mgr.get_all_enabled_games())
-#    is_disabled = list(challenge_mgr.get_all_disabled_games())
-#    
-#    class Meta:
-#        model = GameInfo
-#        fields = ('name','enabled')
-#        widgets = {
-#            'enabled': CheckboxInput(check_test=None)
-#        }
-
-#form = SetGameInfoEnableForm()
-
-# The below code did not work either.
-
-# Need to change name - "contact" was the name used in the examples.
-#def contact(request):
-#    if request.method == 'POST':
-#        #if request.form.isValid():
-#        # Disable enabled apps if checked
-#        for game in games_enabled:
-#            if request.POST.get(game) == True:
-#                # This will cause an error if more than one object has the same name
-#                current_game = GameInfo.objects.get(name=game)
-#                current_game.enabled = 'False'
-#                current_game.save()
-#        # Enable disabled apps if checked
-#        for game in games_disabled:
-#            if request.POST.get(game) == True:
-#                # This will cause an error if more than one object has the same name
-#                current_game = GameInfo.objects.get(name=game)
-#                current_game.enabled = 'True'
-#                current_game.save()
-#        # Update values
-#        games_enabled = list(challenge_mgr.get_all_enabled_games())
-#        games_disabled = list(challenge_mgr.get_all_disabled_games())
-#    # Not sure how to do return statement
-#    return HttpResponseRedirect('.')
                 
